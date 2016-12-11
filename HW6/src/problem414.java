871a08c823d1b7e9bc7272dbf8373443b59a36b2
Brian Martinez <marjosep@msudenver.edu>
2016-12-10 17:07:31 -0700

    uploaded 4.1.4, supporting files

---
+++ HW6/src/problem414.java
@@ -0,0 +1,37 @@
+
+import java.io.FileReader;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Brian Martinez
+ */
+public class problem414 {
+    public static void main(String[] args)
+    {
+        In in = new In("tinyGex2.txt");
+        Graph G = new Graph(in);
+        //System.out.println(G);
+        
+        In pairs = new In("pairs.txt");
+        while (pairs.hasNextLine())
+        {
+            int v = pairs.readInt();
+            int w = pairs.readInt();
+            if (G.hasEdge(v, w))
+            {
+                System.out.println(v + "-" + w + " exists");
+            }
+            else
+            {
+                System.out.println(v + "-" + w + " does not exist");
+            }
+        }
+    }
+}
